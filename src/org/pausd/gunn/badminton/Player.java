c70fa9e23a71ac2dcba858c6151d5da71ba32aba
Tom Monk <tmonk@paypal.com>
2015-12-30 12:01:17 -0800

    Create Match class and TestRunner class

--- src/org/pausd/gunn/badminton/Player.java
+++ src/org/pausd/gunn/badminton/Player.java
@@ -1,77 +1,32 @@
 package org.pausd.gunn.badminton;
 
 public class Player {
+	private final String name;
 	private int rating;
-	private boolean winner;
-	private Player opponent;
-	private int score;
-	
-	public Player(int rating){
-		this.rating = rating;
+
+	public Player(String name) {
+		this.name = name;
 	}
+
 	public int getRating() {
 		return rating;
 	}
+
 	public void setRating(int rating) {
 		this.rating = rating;
 	}
-	
-	public boolean isWinner() {
-		return winner;
+
+	public String getName() {
+		return name;
 	}
-	public void setWinner(boolean winner) {
-		this.winner = winner;
-	}
-	public Player getOpponent() {
-		return opponent;
-	}
-	public void setOpponent(Player opponent) {
-		this.opponent = opponent;
-	}
-	public int getScore() {
-		return score;
-	}
-	public void setScore(int score) {
-		this.score = score;
-	}
-	
-	public void matchUp(Player opponent){
-		this.opponent = opponent;
-		opponent.opponent = this;
-	}
-	public void updateRating(boolean winner, int thisScore, int opponentScore){
-		this.winner = winner;
-		this.score = thisScore;
-		this.opponent.score = opponentScore;
-		
-		if(this.opponent == null){
-			System.out.println("Opponent has not been set yet");
-			return;
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Player) {
+			// Compare our name to the name of the given player
+			return this.getName().equals(((Player)o).getName());
 		}
-		if(this.getRating() > opponent.getRating()){
-			rating = (int) Math.round(rating + ((this.getScore() - opponent.getScore()) - (0.1 * (this.getRating() - opponent.getRating()))));
-			if(winner){
-				rating = rating + 3;
-			}
-		}else{
-			rating = (int) Math.round(rating + ((this.getScore() - opponent.getScore()) + (0.1 * (opponent.getRating() - this.getRating()))));
-			if(winner){
-				rating = rating + 3;
-			}
-		}
+		return false;
 	}
 	
-	//MAKE A PLAYGAME METHOD THAT public playGame(int thisScore, int opponentScore, Player opponent)
-	//that utilizes the updateRating method once determining who has won and lost-and assigning all
-	//the needed values (rating-given, scores-given, winner-calculated)
-	//take out opponent and matchUp();
-	public static void main(String args[]){
-		Player John = new Player(1466);
-		Player Mike = new Player(1430);
-		John.matchUp(Mike);
-		John.updateRating(true,21,5);
-		Mike.updateRating(false,5,21);
-		System.out.println(John.getRating());
-		System.out.println(Mike.getRating());
-	}
 }
