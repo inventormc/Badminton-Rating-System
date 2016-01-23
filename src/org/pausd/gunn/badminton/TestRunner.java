d0aef2196a1bd288d9b87b073f8e5b8c0eb4f33d
Michael Chau <inventormc@gmail.com>
2016-01-22 23:04:18 -0800

    Delete duplicate code

--- src/org/pausd/gunn/badminton/TestRunner.java
+++ src/org/pausd/gunn/badminton/TestRunner.java
@@ -1,6 +1,5 @@
 package org.pausd.gunn.badminton;
 
-import java.util.Date;
 public class TestRunner {
 
 	//MAKE A PLAYGAME METHOD THAT public playGame(int thisScore, int opponentScore, Player opponent)
